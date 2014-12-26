c386b9bf24659f3011872f5525ff99adab014e8f
taukir4u <taukir4u@gmail.com>
2014-12-26 19:09:15 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ ArrayWinApp/PersonNameUI.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ArrayWinApp
+{
+    partial class PersonNameUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.countBox = new System.Windows.Forms.TextBox();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Name";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(95, 31);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(153, 20);
+            this.nameBox.TabIndex = 0;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(275, 30);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(135, 107);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Show All";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Count";
+            // 
+            // countBox
+            // 
+            this.countBox.Location = new System.Drawing.Point(95, 306);
+            this.countBox.Name = "countBox";
+            this.countBox.Size = new System.Drawing.Size(100, 20);
+            this.countBox.TabIndex = 4;
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.Location = new System.Drawing.Point(95, 139);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(153, 147);
+            this.nameListBox.TabIndex = 3;
+            // 
+            // PersonNameUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 338);
+            this.Controls.Add(this.nameListBox);
+            this.Controls.Add(this.countBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label1);
+            this.Name = "PersonNameUI";
+            this.Text = "ArrayWinApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox countBox;
+        private System.Windows.Forms.ListBox nameListBox;
+    }
+}
+
---
+++ ArrayWinApp/PersonNameUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ArrayWinApp
+{
+    public partial class PersonNameUI : Form
+    {
+
+        string[] mylist = new string[10];
+        ArrayList personList = new ArrayList();
+       
+        public PersonNameUI()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            personList.Add(nameBox.Text);
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            nameListBox.Items.Clear();
+            foreach (string aName in personList)
+            {
+                    nameListBox.Items.Add(aName);
+            }
+            
+            countBox.Text = personList.Count.ToString();
+
+        }
+    }
+}
